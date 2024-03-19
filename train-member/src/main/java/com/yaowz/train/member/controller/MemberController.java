e1d756be763bf0b37931428a1ce07da4d1269b8d
姚文政 <959812446@qq.com>
2024-03-19 23:32:30 +0800

    前后端结合login

--- train-member/src/main/java/com/yaowz/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/yaowz/train/member/controller/MemberController.java
@@ -1,14 +1,14 @@
 package com.yaowz.train.member.controller;
 
 import com.yaowz.common.resp.CommonResp;
+import com.yaowz.train.member.req.MemberLoginReq;
 import com.yaowz.train.member.req.MemberRegisterReq;
+import com.yaowz.train.member.req.MemberSendCodeReq;
+import com.yaowz.train.member.resp.MemberLoginResp;
 import com.yaowz.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -28,4 +28,17 @@ public class MemberController {
         long i = memberService.register(req);
         return new CommonResp<>(i);
     }
+
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
+        memberService.sendCode(req);
+        return new CommonResp<>();
+    }
+
+    @PostMapping("/login")
+    public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req) {
+        MemberLoginResp resp = memberService.login(req);
+        return new CommonResp<>(resp);
+    }
+
 }
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -39,8 +39,8 @@
 
 <script>
 import { defineComponent, reactive } from 'vue';
-// import axios from 'axios';
-// import { notification } from 'ant-design-vue';
+import axios from 'axios';
+import { notification } from 'ant-design-vue';
 // import { useRouter } from 'vue-router'
 // import store from "@/store";
 
@@ -54,38 +54,38 @@ export default defineComponent({
       code: '',
     });
 
-    // const sendCode = () => {
-    //   axios.post("/member/member/send-code", {
-    //     mobile: loginForm.mobile
-    //   }).then(response => {
-    //     let data = response.data;
-    //     if (data.success) {
-    //       notification.success({ description: '发送验证码成功！' });
-    //       loginForm.code = "8888";
-    //     } else {
-    //       notification.error({ description: data.message });
-    //     }
-    //   });
-    // };
+    const sendCode = () => {
+      axios.post("http://localhost:8000/member/member/send-code", {
+        mobile: loginForm.mobile
+      }).then(response => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
+    };
 
-    // const login = () => {
-    //   axios.post("/member/member/login", loginForm).then((response) => {
-    //     let data = response.data;
-    //     if (data.success) {
-    //       notification.success({ description: '登录成功！' });
-    //       // 登录成功，跳到控台主页
-    //       router.push("/welcome");
-    //       store.commit("setMember", data.content);
-    //     } else {
-    //       notification.error({ description: data.message });
-    //     }
-    //   })
-    // };
+    const login = () => {
+      axios.post("http://localhost:8000/member/member/login", loginForm).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '登录成功！' });
+          // 登录成功，跳到控台主页
+          // router.push("/welcome");
+          // store.commit("setMember", data.content);
+        } else {
+          notification.error({ description: data.message });
+        }
+      })
+    };
 
     return {
       loginForm,
-      // sendCode,
-      // login
+      sendCode,
+      login
     };
   },
 });
