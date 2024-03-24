25fbc186a595ed0ee3072962148e8243840490db
姚文政 <959812446@qq.com>
2024-03-25 00:01:08 +0800

    乘车人新增接口

---
+++ train-member/src/main/java/com/yaowz/train/member/controller/PassengerController.java
@@ -0,0 +1,52 @@
+package com.yaowz.train.member.controller;
+
+
+import com.yaowz.common.resp.CommonResp;
+import com.yaowz.train.member.req.PassengerSaveReq;
+import com.yaowz.train.member.service.PassengerService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/passenger")
+public class PassengerController {
+
+    @Resource
+    private PassengerService passengerService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
+        passengerService.save(req);
+        return new CommonResp<>();
+    }
+
+//    @GetMapping("/query-list")
+//    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
+//        req.setMemberId(LoginMemberContext.getId());
+//        PageResp<PassengerQueryResp> list = passengerService.queryList(req);
+//        return new CommonResp<>(list);
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public CommonResp<Object> delete(@PathVariable Long id) {
+//        passengerService.delete(id);
+//        return new CommonResp<>();
+//    }
+//
+//    @GetMapping("/query-mine")
+//    public CommonResp<List<PassengerQueryResp>> queryMine() {
+//        List<PassengerQueryResp> list = passengerService.queryMine();
+//        return new CommonResp<>(list);
+//    }
+//
+//    @GetMapping("/init")
+//    public CommonResp<Object> init() {
+//        passengerService.init();
+//        return new CommonResp<>();
+//    }
+
+}
---
+++ train-member/src/main/java/com/yaowz/train/member/service/PassengerService.java
@@ -0,0 +1,40 @@
+package com.yaowz.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.yaowz.common.util.SnowUtil;
+import com.yaowz.train.member.domain.Passenger;
+import com.yaowz.train.member.mapper.MemberMapper;
+import com.yaowz.train.member.mapper.PassengerMapper;
+import com.yaowz.train.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    @Resource
+    private MemberMapper memberMapper;
+
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        if (ObjectUtil.isNull(passenger.getId())) {
+//            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+    }
+}
