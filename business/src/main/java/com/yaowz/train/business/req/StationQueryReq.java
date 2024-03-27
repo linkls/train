069c5a5db3d6f68710c4b7019af1e160c7bd3265
姚文政 <959812446@qq.com>
2024-03-27 23:43:15 +0800

    生成station

---
+++ business/src/main/java/com/yaowz/train/business/req/StationQueryReq.java
@@ -0,0 +1,12 @@
+package com.yaowz.train.business.req;
+
+import com.yaowz.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}
---
+++ business/src/main/java/com/yaowz/train/business/service/StationService.java
@@ -0,0 +1,70 @@
+package com.yaowz.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.yaowz.common.resp.PageResp;
+import com.yaowz.common.util.SnowUtil;
+import com.yaowz.train.business.domain.Station;
+import com.yaowz.train.business.domain.StationExample;
+import com.yaowz.train.business.mapper.StationMapper;
+import com.yaowz.train.business.req.StationQueryReq;
+import com.yaowz.train.business.req.StationSaveReq;
+import com.yaowz.train.business.resp.StationQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StationService.class);
+
+    @Resource
+    private StationMapper stationMapper;
+
+    public void save(StationSaveReq req) {
+        DateTime now = DateTime.now();
+        Station station = BeanUtil.copyProperties(req, Station.class);
+        if (ObjectUtil.isNull(station.getId())) {
+            station.setId(SnowUtil.getSnowflakeNextId());
+            station.setCreateTime(now);
+            station.setUpdateTime(now);
+            stationMapper.insert(station);
+        } else {
+            station.setUpdateTime(now);
+            stationMapper.updateByPrimaryKey(station);
+        }
+    }
+
+    public PageResp<StationQueryResp> queryList(StationQueryReq req) {
+        StationExample stationExample = new StationExample();
+        stationExample.setOrderByClause("id desc");
+        StationExample.Criteria criteria = stationExample.createCriteria();
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Station> stationList = stationMapper.selectByExample(stationExample);
+
+        PageInfo<Station> pageInfo = new PageInfo<>(stationList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<StationQueryResp> list = BeanUtil.copyToList(stationList, StationQueryResp.class);
+
+        PageResp<StationQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        stationMapper.deleteByPrimaryKey(id);
+    }
+}
